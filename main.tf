4180077c538581b39f8555499a285df6daf488d5
Scott Miller <scott@millergeek.xyz>
2019-06-05 13:50:14 -0400

    fix dependencies

--- main.tf
+++ main.tf
@@ -489,7 +489,7 @@ resource "aws_ecs_task_definition" "atlantis" {
 
 data "aws_ecs_task_definition" "atlantis" {
   task_definition = var.name
-  dependencies = [aws_ecs_task_definition.atlantis]
+  depends_on = [aws_ecs_task_definition.atlantis]
 }
 
 resource "aws_ecs_service" "atlantis" {
