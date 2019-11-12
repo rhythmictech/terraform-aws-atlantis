daf69b3ba9292c0d662a6483db2bb7d7dc0cc73e
Cris Daniluk <cdaniluk@rhythmictech.com>
2019-11-12 14:43:29 -0500

    working off fork pending merge of PR

--- main.tf
+++ main.tf
@@ -290,7 +290,7 @@ module "acm" {
 #  source  = "terraform-aws-modules/acm/aws"
   # pending merge of https://github.com/terraform-aws-modules/terraform-aws-acm/pull/28
   source = "git::https://github.com/rhythmictech/terraform-aws-acm.git?ref=master"
-  version = "v2.0.0"
+#  version = "v2.0.0"
 
   create_certificate = var.certificate_arn == ""
 
